3f094398efadfb0b1fee11c41d1421a46a818cb9
HrayrManukyan <59926659+HrayrManukyan@users.noreply.github.com>
2020-01-15 20:49:03 +0400

    First commit

---
+++ PY.py
@@ -0,0 +1,41 @@
+print("Hello, World!")
+print(5)
+print(5.1)
+print(True)
+print(False)
+
+print("Hrayr")
+print("Manukyan")
+print("27")
+print("some dummy text")
+
+CSR = "Corporate social responcibility 2"
+CMD = "Command"
+
+print (CSR,CMD)
+
+type("major")
+type("middle")
+type("minor")
+
+my_number=(44)
+
+
+print(type (my_number))
+
+str_my_number=str(my_number)
+
+print(type(str_my_number))
+
+int_my_number = int(my_number)
+print (type(int_my_number))
+
+float_my_number = float(my_number)
+print (type(float_my_number))
+
+bool_my_number = bool (my_number)
+print (type(bool_my_number))
+
+print (float_my_number)
+
+print (str_my_number)
